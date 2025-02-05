5f95104151d712e102019e3ae7382713d9cb9649
rkbart <rk_bart@yahoo.com>
2025-02-06 02:27:58 +0800

    generated Task and Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Category.create(name: "Work")
+Category.create(name: "Personal")
+Category.create(name: "Shopping")
