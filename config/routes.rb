65eb9ace2dddc5211e3ae9c9cf2c0fa07b0d662d
rkbart <rk_bart@yahoo.com>
2025-02-17 19:45:00 +0800

    cleaning codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,3 @@
-# Rails.application.routes.draw do
-#   get "/login", to: "sessions#new"
-#   post "/login", to: "sessions#create"
-#   delete "/logout", to: "sessions#destroy"
-#   get "/logout", to: "sessions#destroy"
-
-
-#   resources :users
-
-#   resources :categories do
-#     resources :tasks, only: [:index, :new, :create, :edit, :update, :destroy, :show]
-#     get 'all_tasks', on: :collection 
-#   end
-
-  # root 'categories#index'
-  # root 'sessions#new'
-# end
-
 Rails.application.routes.draw do
   # Session routes
   get "/login", to: "sessions#new"
