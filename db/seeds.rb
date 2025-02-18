f832fc506ad72bd330bfb38add966c8ce7f1880c
rkbart <rk_bart@yahoo.com>
2025-02-18 19:05:19 +0800

    return to og seed for main

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ user = User.find_or_create_by!(email: 'rk_bart@yahoo.com') do |u|
     u.username = 'Test User'
   end
 Category.find_or_create_by!(name: "Uncategorized", user: user)
-# Category.find_or_create_by!(name: "Work", user: user)
-# Category.find_or_create_by!(name: "Personal", user: user)
+Category.find_or_create_by!(name: "Work", user: user)
+Category.find_or_create_by!(name: "Personal", user: user)
 
-# work_category = Category.find_by(name: "Work")
-# Task.create!(title: "Sample Task", description: "A test task", due_date: Date.today, completed: false, category: work_category)
+work_category = Category.find_by(name: "Work")
+Task.create!(title: "Sample Task", description: "A test task", due_date: Date.today, completed: false, category: work_category)
